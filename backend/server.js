33b98d8cd3513736444a625dc125fcc39c058d63
Nameson Gaudel <namesongaudel.ng@gmail.com>
2022-06-02 10:33:42 +0545

    image upload route is setup using multer

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 import express from "express";
+import path from 'path'
+import dotenv from "dotenv";
 
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+import uploadeImage from './uplodeIMG.js';
+
+// api routes
+app.use("/uploadImage", uploadeImage);
+
+
+//To send static image by src link
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const Port = 5000;
 app.listen(Port, console.log(`Server is running on port ${Port}`));
